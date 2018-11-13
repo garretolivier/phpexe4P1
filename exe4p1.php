6ecfa724e082dda31dcc6da49f4db3351b0376b6
garretolivier <garretolivier@gmail.com>
2018-11-13 14:18:33 +0100

    exe4P1

---
+++ exe4p1.php
@@ -0,0 +1,24 @@
+<!-- Créer une variable de type string, une variable de type int, 
+une variable de type float, une variable de type booléan et 
+les initialiser avec une valeur de votre choix.
+Les afficher.
+-->
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>PHP Exercice 4 Partie 1</title>
+    </head>
+    <body>
+        <?php
+       $name = 'garret olivier'; /*variable de type string*/
+       $age = 18; /*variable de type int*/
+       $weight = 80.55; /*variable de type float*/
+       $je_suis_bon_en_php = true; /*varaible de type boolean*/
+       $je_suis_mauvais_php_ = false; /*variable de type boolean*/
+       echo 'je m\'appel ' . $name . ' j\'ai ' . $age . ' je pèse ' . $weight . ' je suis un beau gosse ' . $je_suis_bon_en_php;
+       ?>
+    </body>
+</html>
+
